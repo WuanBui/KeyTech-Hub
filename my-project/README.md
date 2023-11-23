1b693704339b16ee7e7a42e3240c7d5582bbdc12
WuanBui <118819560+WuanBui@users.noreply.github.com>
2023-11-24 03:06:33 +0700

    24/11/2023

---
+++ my-project/README.md
@@ -0,0 +1,8 @@
+*Sử dụng: Daisy UI, Vite + tailwindcss
+*Setup môi trường
+*Tạo các trang
+*Route structure
+*Tạo trang error
+*Tạo form input
+*Tạo trang login 
+*Chỉnh sửa nút submit
---
+++ my-project/src/App.jsx
@@ -0,0 +1,76 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import {
+  About,
+  Cart,
+  Checkout,
+  Error,
+  HomeLayout,
+  Landing,
+  Login,
+  Orders,
+  Products,
+  Register,
+  SingleProduct,
+} from "./pages";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+      },
+
+      {
+        path: "product",
+        element: <Products />,
+      },
+
+      {
+        path: "product/:id",
+        element: <SingleProduct />,
+      },
+
+      {
+        path: "cart",
+        element: <Cart />,
+      },
+
+      {
+        path: "about",
+        element: <About />,
+      },
+
+      {
+        path: "checkout",
+        element: <Checkout />,
+      },
+
+      {
+        path: "orders",
+        element: <Orders />,
+      },
+    ],
+  },
+
+  {
+    path: "/login",
+    element: <Login />,
+    errorElement: <Error />,
+  },
+
+  {
+    path: "/register",
+    element: <Register />,
+    errorElement: <Error />,
+  },
+]);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
+export default App;
